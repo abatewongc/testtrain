34fc0e732d757937b9d2b9595a2916da7dced359
Christian Abate-Wong <abatewongc@wit.edu>
2018-06-05 21:35:43 -0400

    got the resizer to appear

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,22 +7,18 @@ import styles from './Home.css';
 type Props = {};
 
 export default class Home extends Component<Props> {
-  props: Props;
+	props: Props;
 
-  render() {
-    return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <SplitPane split="vertical" defaultSize="50%">
-            <div>
-              <p>Stuff</p>
-            </div>
-            <div>
-              <p>More Stuff</p>
-            </div>
-          </SplitPane>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div className={styles.container} data-tid="container">
+					<SplitPane split="vertical" defaultSize={300} minSize={200} maxSize={-500}>
+						<div><p>Stuff</p></div>
+						<div><p>More Stuff</p></div>
+					</SplitPane>
+				</div>
+			</div>
+		);
+	}
 }
--- app/menu.js
+++ app/menu.js
@@ -28,7 +28,6 @@ export default class MenuBuilder {
   }
 
   setupDevelopmentEnvironment() {
-    this.mainWindow.openDevTools();
     this.mainWindow.webContents.on('context-menu', (e, props) => {
       const { x, y } = props;
 
