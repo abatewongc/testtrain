97140d0729ba6c8b80c65f38fa12be59a47e5fad
Christian Abate-Wong <abatewongc@wit.edu>
2018-06-16 18:18:09 -0400

    got the test case display container working

--- app/components/TestProjectViewer/TestProjectViewer.js
+++ app/components/TestProjectViewer/TestProjectViewer.js
@@ -6,6 +6,11 @@ import EndpointViewer from '../EndpointViewer/EndpointViewer'
 import { Z_FIXED } from 'zlib';
 import { inherits } from 'util';
 import { loadEndpoint, clearEndpoint } from "../../actions/endpoint-viewer";
+import { Collapse } from 'antd';
+const Panel = Collapse.Panel;
+import paths from 'path'
+
+import Testcase from '../Testcase/Testcase'
 
 const mapStateToProps = state => {
     return { endpoint: state.current_endpoint_reducer.current_endpoint.endpoint };
@@ -30,17 +35,84 @@ class ConnectedTestProjectViewer extends React.Component {
 	    cases: []
     }
 
+		this.extractTestcasesFromTEF = this.extractTestcasesFromTEF.bind(this);
 		this.renderEndpointData = this.renderEndpointData.bind(this);
-  }
+		this.createTestCaseObject = this.createTestCaseObject.bind(this);
+	}
+
+	createTestCaseObject(endpoint) {
+		// ************************************************************** */
+		// THIS IS WHERE WE WOULD GET THE DATA FROM THE TEF
+		// ANY HARDCODED DATA IN THIS METHOD WOULD BE EXTRACTED IN THIS BLOCK
+		let _name = 'experimental'
+		// ************************************************************** */
+		let testcase = {
+			name: _name,
+			fullurl: paths.join(endpoint.name.replace(new RegExp('&', 'g'), '/'), '/', _name),
+			success: endpoint.data.successCode,
+			fail: endpoint.data.failCode,
+			successoutput: [
+				{
+					text: "experimental_success"
+					// other stuff?
+				}
+			],
+			failoutput:[
+				{
+					text: "experimental_failure"
+					// other this?
+				}
+			],
+			input: [
+				{
+					parameter: "1",
+					type: "string",
+					value: "hi",
+				},
+				{
+					parameter: "2",
+					type: "integer",
+					value: "2",
+				},{
+					parameter: "3",
+					type: "boolean",
+					value: "0",
+				}
+			]
+
+		}
+
+		return testcase;
+	}
+
+	extractTestcasesFromTEF(endpoint) {
+		let testcase1 = this.createTestCaseObject(endpoint);
+		let testcase2 = this.createTestCaseObject(endpoint);
+		let testcase3 = this.createTestCaseObject(endpoint);
+		let testcase4 = this.createTestCaseObject(endpoint);
+		let testcase5 = this.createTestCaseObject(endpoint);
+
+		return [ testcase1, testcase2, testcase3, testcase4, testcase5 ];
+	}
 
 	renderEndpointData(endpoint) {
 		if(endpoint) {
-			return <p>{JSON.stringify(endpoint.data)}</p>
+			let testcases = this.extractTestcasesFromTEF(endpoint);
+			if(testcases == null) {
+				return (
+					<p>{JSON.stringify(endpoint.data)}</p>
+				);
+			} else {
+					return (
+						<Collapse>
+						{testcases.map((tc, index) => (<Testcase testcase={tc} key={tc.name.concat('_testcase_').concat(index)} />))}
+						</Collapse>
+					);
+			}
 		}
 	}
 
 	render() {
-		console.log(this.props.endpoint);
 
 		return (
 	    <Layout style={{ marginLeft: 0 , height: '100vh'}}>
