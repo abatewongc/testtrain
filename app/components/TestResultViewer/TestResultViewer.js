375a5611b98b93d0b3588214d2ddc39fa8b938e3
Christian Abate-Wong <abatewongc@wit.edu>
2018-07-23 17:26:30 -0400

    report viewer now opens in the browser

--- app/components/TestResultViewer/TestResultViewer.js
+++ app/components/TestResultViewer/TestResultViewer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Spin } from 'antd';
+import { Spin, Button } from 'antd';
 import paths from 'path';
 var fs = require('fs');
 
@@ -27,38 +27,46 @@ class ConnectedTestResultViewer extends React.Component {
             reportIsLoaded: false,
             report: '',
         }
+
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    handleButtonClick = () => {
+        console.log(this.state.reportPath);
+        require('electron').shell.openExternal(this.state.reportPath);
     }
 
     componentDidMount() {
         let { tefPath } = this.props.endpoint;
         // this is called every time the modal is opened, so we can find the latest report and load it now
         // the latest report is located in a subdirectory on the same level...
-        let reportPath = paths.join(tefPath, "../reports/latest.html");
+        let reportPath = paths.join(tefPath, "../../../mochawesome-report/latest.html");
+        console.log(reportPath);
         this.setState({ reportPath });
         fs.readFile(reportPath, 'utf-8', (err, data) => {
             if(!err) {
-                this.setState({ report: data });
                 this.setState({ reportIsLoaded: true });
             }
         });
     }
-    
+
 
 	render() {
-        if(this.state.reportIsLoaded) {
+
                 const { report } = this.state;
                 const html = { __html: report };
                 return(
-                    <div>
-                        <div dangerouslySetInnerHTML={html}></div>
-                    </div>
+                    <div style={{ textAlign: 'center' }}><Button
+                    type="primary"
+                    icon="plus"
+                    size="default"
+                    onClick={this.handleButtonClick}
+                    disabled={!this.state.reportIsLoaded}
+                    >
+                        View in Browser
+                    </Button></div>
                 );
-        } else {
-            return(
-                <div style={{ textAlign: 'center' }}><Spin /></div>
-               
-            );
-        }
+
     }
 }
 
